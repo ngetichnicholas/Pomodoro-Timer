9f93ce88e5423e8a2034d76d718c59005ef56fab
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-15 01:19:25 +0300

    Update config

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
+import os
 class Config:
     '''
     General configuration parent class
     '''
-    pass
-
-
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
     '''
@@ -13,8 +13,9 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    pass
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+      SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",1)
 
 class DevConfig(Config):
     '''
@@ -23,5 +24,14 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:nick101010@localhost/pomodoro'
     DEBUG = True
+
+class TestConfig(Config):
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:nick101010@localhost/pomodoro_test'
+
+config_options = {
+  'production':ProdConfig,
+  'development':DevConfig,
+  'test':TestConfig
+}
