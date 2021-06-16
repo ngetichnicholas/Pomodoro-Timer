4c13e0f8988ccf6a3a70c6498bc4a269b1f0687e
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-16 11:24:29 +0300

    Add model class for Task objects

--- app/models.py
+++ app/models.py
@@ -17,3 +17,40 @@ class User(UserMixin,db.Model):
   bio = db.Column(db.String(255))
   profile_pic_path = db.Column(db.String())
   tasks = db.relationship('Task', backref='user', lazy='dynamic')
+
+  @property
+  def set_password(self):
+    raise AttributeError('You cannot read the password attribute')
+
+  @set_password.setter
+  def password(self, password):
+    self.secure_password = generate_password_hash(password)
+
+  def verify_password(self, password):
+    return check_password_hash(self.secure_password,password) 
+  
+  def save_u(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+  
+  def __repr__(self):
+    return f'User {self.username}'
+
+class Task(db.Model):
+  __tablename__ = 'tasks'
+  id = db.Column(db.Integer, primary_key = True)
+  title = db.Column(db.String(255),nullable = False)
+  task = db.Column(db.Text(), nullable = False)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+  time = db.Column(db.DateTime, default = datetime.utcnow)
+  
+  def save_p(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def __repr__(self):
+    return f'Task {self.post}'
